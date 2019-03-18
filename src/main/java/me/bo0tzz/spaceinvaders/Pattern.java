d9f61485767e9914ce855fcb0d392a55e4d4266a
Bo0tzz <btdewilligen@gmail.com>
2019-03-19 00:09:52 +0100

    Tweaks to windowAt method

--- src/main/java/me/bo0tzz/spaceinvaders/Pattern.java
+++ src/main/java/me/bo0tzz/spaceinvaders/Pattern.java
@@ -32,29 +32,40 @@ public class Pattern {
 
     public Stream<Coordinate> coordinateStream() {
 
-        Set<Integer> xCoords = ContiguousSet.closed(0, this.width());
-        Set<Integer> yCoords = ContiguousSet.closed(0, this.height());
+        Set<Integer> xCoords = ContiguousSet.closed(0, this.width() - 1);
+        Set<Integer> yCoords = ContiguousSet.closed(0, this.height() - 1);
 
         return Sets.cartesianProduct(xCoords, yCoords).stream()
                 .map(list -> new Coordinate(list.get(0), list.get(1)));
 
     }
 
+    /**
+     * Get a window into a part of this Pattern
+     * @param coordinate the top-left coordinate of the window
+     * @param width the width of the window
+     * @param height the height of the window
+     * @return a window into this Pattern
+     */
     public Pattern windowAt(Coordinate coordinate, int width, int height) {
+
+        //TODO: Add wraparound
 
         if (width < 0 || height < 0) {
             throw new IllegalArgumentException("Negative window size is not allowed");
         }
 
         boolean[][] window =  new boolean[height][width];
+        int destY = 0;
 
         //For every line in our window
-        for (int y = coordinate.getY(); y < coordinate.getY() + height; y++) {
+        for (int y = coordinate.getY(); y < coordinate.getY() + height - 1; y++) {
             //Copy the columns in our window to the output array
             int pos = coordinate.getX();
-            boolean[] src = pattern[y];
-            boolean[] dest = window[y];
-            System.arraycopy(src, pos, dest, pos, width);
+            boolean[] src = pattern[y]; //FIXME: IOOBE if window overlaps pattern borders; implement wraparound
+            boolean[] dest = window[destY];
+            System.arraycopy(src, pos, dest, destY, width);
+            destY++;
         }
 
         return new Pattern(window);
